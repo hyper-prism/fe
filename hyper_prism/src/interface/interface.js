e0b3922e2f8562e6c1045a79d365b81eb4969f53
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-03-23 00:51:19 -0500

    added color changing functionality for buttons and have it updating state

--- hyper_prism/src/interface/interface.js
+++ hyper_prism/src/interface/interface.js
@@ -5,27 +5,30 @@ export default class Interface extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            button1: {clicked: false, number: 1, name: 'button1'},
-            button2: {clicked: false, number: 2, name: 'button2'},
-            button3: {clicked: false, number: 3, name: 'button3'}
-        }
+            nums: {
+                button1: {number: 1, name: 'button1'},
+                button2: {number: 2, name: 'button2'},
+                button3: {number: 3, name: 'button3'},}
+            }
     }
 
     buttonHandler = (e)  => {
-        for(let item in this.state){
-           if(item !== e.name){
-               console.log(item)
-               this.setState({
-                   [item]: {clicked: false},
-               })
-           }   
+        let selectedNum = 0
+        for(let item in this.state.nums){
+            if(item !== e.name){
+                document.querySelector(`.${item}`).style.background = 'black'
+            }                  
+            document.querySelector(`.${e.name}`).style.background = 'green'
+            selectedNum = e.number            
         }
-     console.log(e)
+        this.submitHandler(selectedNum)
+    }
+
+    submitHandler = (num) => {
+        console.log(num)
     }
 
     render(){
-        console.log(this.state)
-
         return(
             <div>
                 <h1 class="header">Hyper Prism</h1>
@@ -35,13 +38,13 @@ export default class Interface extends React.Component{
                 </div> 
                 <div className='controls-container'> 
                     <div class="numbers"> 
-                        <div onClick={this.buttonHandler.bind(this, this.state.button1)}>
+                        <div onClick={this.buttonHandler.bind(this, this.state.nums.button1)}>
                             <h1 className="choice1 button1">I</h1>
                         </div> 
-                        <div onClick={this.buttonHandler.bind(this, this.state.button2)}> 
+                        <div onClick={this.buttonHandler.bind(this, this.state.nums.button2)}> 
                             <h1 class="choice1 button2" >II</h1>
                         </div> 
-                        <div onClick={this.buttonHandler.bind(this, this.state.button3)}> 
+                        <div onClick={this.buttonHandler.bind(this, this.state.nums.button3)}> 
                             <h1 class="choice1 button3">III</h1>
                         </div> 
                     </div>  
