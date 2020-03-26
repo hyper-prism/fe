3d0fd3cc9aca6fbb2c960b511e3162246ee0695c
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-03-25 23:50:24 -0500

    added function that updates prism after clicks

--- hyper_prism/src/interface/interface.js
+++ hyper_prism/src/interface/interface.js
@@ -10,7 +10,8 @@ export default class Interface extends React.Component{
                 button2: {number: 2, name: 'button2'},
                 button3: {number: 3, name: 'button3'},
             },
-            score: 0
+            score: 0, 
+            turns: 0
             }
     }
 
@@ -28,24 +29,54 @@ export default class Interface extends React.Component{
 
     submitHandler = (num, score) => {
         let random = Math.floor(Math.random() * 3) + 1; 
+        console.log(random)
         if(random === num){
+            score = score + 1
+            turns = turns + 1
+            if(score === 1){
+                this.updatePrism('150px', 'blue', '100px', '100px')
+            } else if(score === 2){
+                this.updatePrism('150px', 'blue', '100px', '100px')
+            } else if(score === 3){
+                this.updatePrism('150px', 'blue', '100px', '100px')            } 
             this.setState({
-                score: score +=1
-            }) 
+                score: score,
+                turns: turns
+            })
+
         } else if(num !== random){
+            score = score -= 1
+            if(score === -1){
+                this.updatePrism('150px', 'blue', '100', '100')
+
+            } else if(this.state.score === -2){
+                document.querySelector('.triangle').style.background = 'blue'
+            } else if(this.state.score === -3){
+                document.querySelector('.triangle').classList.add('wrong-3')
+            } 
             this.setState({
-                score: score -=1
+                score: score
             })
         }
     }
 
-    render(){
+    updatePrism = (bb, color, borderLR,) => {
+        let prism = document.querySelector('.triangle')
+        prism.style.borderBottom = `${bb} solid ${color}`;
+        prism.style.borderLeft = `${borderLR} solid transparent`;
+        prism.style.borderRight = `${borderLR} solid transparent`;
+        prism.style.transition = "3s";
+    }
+
+    render(){        
+console.log(this.state.score)
+
         return(
             <div>
                 <h1 class="header">Hyper Prism</h1>
                 <hr style={{width:'100%', borderBottomColor: 'green', boxShadow: "1px 2px 2px 1px green"}} /> 
                 <div class="container">
-                    <div class="triangle"></div> 
+                    <div className="triangle"></div> 
                 </div> 
                 <div className='controls-container'> 
                     <div class="numbers"> 
