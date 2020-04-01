92e016b3a14b43793b0013b99293495880e5f9e9
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-03-31 23:19:42 -0500

    set up authentication on the front end, completed folders, files, and boilerplate for leaderboard.js and forms

---
+++ hyper_prism/src/leaderboard/leaderboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class LeaderBoard extends React.Component{
+    constructor(props){
+        super()
+    }
+    render(){
+        return(
+            <div>
+                Leaderboard
+            </div> 
+        )
+    }
+}
