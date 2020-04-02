e19817ef8bcf41462ab56a9d02b736fa97832f05
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-04-01 23:20:14 -0500

    set up and connected forms for login and register

--- hyper_prism/src/forms/login.js
+++ hyper_prism/src/forms/login.js
@@ -10,10 +10,42 @@ export default class Login extends React.Component{
 
         }
     }
+
+    changeHandler = (event) => {
+        event.preventDefault()
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault()
+        console.log(this.state)
+    }
     
     render(){
         return(
-            <div>Login</div>
+            <form>
+                <input placeholder='Username' 
+                       name='username' 
+                       value={this.state.username} 
+                       onChange={this.changeHandler}
+                       type='text'
+                />
+                <input placeholder='Email'
+                       name='email'
+                       type='text'
+                       onChange={this.changeHandler}
+                       value={this.state.email}
+                />
+                <input name='password'
+                       type='password'
+                       placeholder='Password'
+                       value={this.state.password}
+                       onChange={this.changeHandler}
+                />
+                <div onClick={this.submitHandler}>Submit</div>
+            </form>
         )
     }
 }
--- hyper_prism/src/forms/register.js
+++ hyper_prism/src/forms/register.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../styles/forms/register.css'
 
 export default class Register extends React.Component{
     constructor(){
@@ -10,10 +11,42 @@ export default class Register extends React.Component{
 
         }
     }
+
+    changeHandler = (event) => {
+        event.preventDefault()
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault()
+        console.log(this.state)
+    }
     
     render(){
         return(
-            <div>REGISTER</div>
+            <form>
+                <input placeholder='Username' 
+                       name='username' 
+                       value={this.state.username} 
+                       onChange={this.changeHandler}
+                       type='text'
+                />
+                <input placeholder='Email'
+                       name='email'
+                       type='text'
+                       onChange={this.changeHandler}
+                       value={this.state.email}
+                />
+                <input name='password'
+                       type='password'
+                       placeholder='Password'
+                       value={this.state.password}
+                       onChange={this.changeHandler}
+                />
+                <div onClick={this.submitHandler}>Submit</div>
+            </form>
         )
     }
 }
