b00d8563f467006c5aab832c8c4acc7c240498a0
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-04-28 23:29:04 -0500

    got leaderboard to render properly and adjusted spacing with css

--- hyper_prism/src/leaderboard/leaderboard.js
+++ hyper_prism/src/leaderboard/leaderboard.js
@@ -6,18 +6,28 @@ export default class LeaderBoard extends React.Component{
         super(props)
     }
     render(){
+
+        console.log(this.props.leaderBoard)
         return(
             <div>
                 <div>
                     <h1 className="header">Hyper Prism</h1>                
                     <hr style={{width:'100%', borderBottomColor: 'green', boxShadow: "1px 2px 2px 1px green", marginBottom: '0px'}} /> 
                 </div>
-                <div className='grand-container'>
-                    <div className='menu-container'>
+                    <div className='menu-container' >
                         <h2 style={{color: 'white', textDecoration: 'underline'}}>Hall of Records</h2>
-  
+                        <div style={{width: '100%', overflow: 'scroll', height: '400px'}}> 
+                        {this.props.leaderBoard.map((item, index) => (
+                            index < 20 ?
+                                <div key={index} style={{ display: 'flex', flexWrap: 'wrap', width: '100%', alignItems: 'center', justifyContent: 'space-evenly' }}>
+                                    <p>{item['users.username']}</p> 
+                                    <p style={{color: 'white'}}>{item.score}</p>
+                                </div>                                 
+                            : 'loading'
+
+                        ))}
+                        </div>
                     </div>
-                </div>  
                 <div className='controls-container'> 
                     <div className="numbers"> 
                         <div>
