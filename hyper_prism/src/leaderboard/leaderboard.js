7dcd3f73ff24567d062071302dff6413d9444781
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-04-28 22:57:46 -0500

    created template for leaderboard. Just need to add actual board

--- hyper_prism/src/leaderboard/leaderboard.js
+++ hyper_prism/src/leaderboard/leaderboard.js
@@ -1,13 +1,40 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 export default class LeaderBoard extends React.Component{
     constructor(props){
-        super()
+        super(props)
     }
     render(){
         return(
             <div>
-                Leaderboard
+                <div>
+                    <h1 className="header">Hyper Prism</h1>                
+                    <hr style={{width:'100%', borderBottomColor: 'green', boxShadow: "1px 2px 2px 1px green", marginBottom: '0px'}} /> 
+                </div>
+                <div className='grand-container'>
+                    <div className='menu-container'>
+                        <h2 style={{color: 'white', textDecoration: 'underline'}}>Hall of Records</h2>
+  
+                    </div>
+                </div>  
+                <div className='controls-container'> 
+                    <div className="numbers"> 
+                        <div>
+                            <h1 className="choice1 button1">I</h1>
+                        </div> 
+                        <div> 
+                            <h1 className="choice1 button2" >II</h1>
+                        </div> 
+                        <div> 
+                            <h1 className="choice1 button3">III</h1>
+                        </div> 
+                    </div>  
+                    <div className="controls">
+                        <div onClick={this.restartHandler} className="restart">Restart</div>
+                        <div type='submit'><NavLink style={{textDecoration: "none", fontWeight: "800", color: "white"}} to='/'>Return</NavLink></div>
+                    </div>
+                </div>   
             </div> 
         )
     }
