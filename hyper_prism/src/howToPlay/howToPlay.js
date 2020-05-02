ab892342c5d7becef911fa8242fa044b5b5031d1
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-05-01 19:15:13 -0500

    added some styling to how to play

--- hyper_prism/src/howToPlay/howToPlay.js
+++ hyper_prism/src/howToPlay/howToPlay.js
@@ -14,19 +14,20 @@ export default class HowToPlay extends React.Component{
                     <hr style={{width:'100%', borderBottomColor: 'green', boxShadow: "1px 2px 2px 1px green", marginBottom: '0px'}} /> 
                 </div> 
                     <div className='menu-container'>
-                        <h3>How To Play</h3>
-                        <hr/> 
-                        <p>1) Select a number at the bottom of the screen</p> 
-                        <p>2) The computer will simultaneously select a number</p> 
-                        <p>3) If your number is the same as the computers selection, your hyper prism grows.</p> 
-                        <p>4) If it's not the same, your hyper prism fades away</p> 
-                        <p>5) Once big enough, you'll be able to suspend your hyper prism, meaning you've won the round.</p> 
-                        <p>6) Careful! If your hyper prism gets too small, it just may float away</p> 
-                        <p>7) Based on how many attempts it takes to complete the round, a score will be created</p> 
-                        <p>8) Anything beyond 9 attempts will be awarded a 1</p>
-                        <p>9) Anything less that 9 attempts is awarded the current attempt deducted from 9 (ex. 9 - 3 = 6, because I won in 3 attempts).</p> 
-                        <p>10) If you do not win a round, no points are awarded.</p> 
-                        <p>11) Only those who create an account will have their high scores logged to the leader board.</p> 
+                        <h3 style={{marginBottom: '0px'}}>How To Play</h3>
+                        <div style={{overflow: 'scroll', height: '400px', width: '80%'}}> 
+                            <p style={{textAlign: 'left', fontSize: '12px', fontWeight: 'bold'}}>Select a number at the bottom of the screen</p> 
+                            <p style={{textAlign: 'left', fontSize: '12px', fontWeight: 'bold'}}>The computer will simultaneously select a number</p> 
+                            <p style={{textAlign: 'left', fontSize: '12px', fontWeight: 'bold'}}>If your number is the same as the computers selection, your hyper prism grows.</p> 
+                            <p style={{textAlign: 'left', fontSize: '12px', fontWeight: 'bold'}}>If it's not the same, your hyper prism fades away</p> 
+                            <p style={{textAlign: 'left', fontSize: '12px', fontWeight: 'bold'}}>Once big enough, you'll be able to suspend your hyper prism, meaning you've won the round.</p> 
+                            <p style={{textAlign: 'left', fontSize: '12px', fontWeight: 'bold'}}>Careful! If your hyper prism gets too small, it just may float away</p> 
+                            <p style={{textAlign: 'left', fontSize: '12px', fontWeight: 'bold'}}>Based on how many attempts it takes to complete the round, a score will be created</p> 
+                            <p style={{textAlign: 'left', fontSize: '12px', fontWeight: 'bold'}}>Anything beyond 9 attempts will be awarded a 1</p>
+                            <p style={{textAlign: 'left', fontSize: '12px', fontWeight: 'bold'}}>Anything less that 9 attempts is awarded the current attempt deducted from 9 (ex. 9 - 3 = 6, because I won in 3 attempts).</p> 
+                            <p style={{textAlign: 'left', fontSize: '12px', fontWeight: 'bold'}}> If you do not win a round, no points are awarded.</p> 
+                            <p style={{textAlign: 'left', fontSize: '12px', fontWeight: 'bold'}}> Only those who create an account will have their high scores logged to the leader board.</p> 
+                        </div> 
                     </div> 
                 <div className='controls-container'> 
                     <div className="numbers"> 
